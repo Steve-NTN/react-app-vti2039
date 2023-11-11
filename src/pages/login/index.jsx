c053facdd0e283ac4f4caaa32f338d7736ac1f87
anhnghia47 <trongnghiafes@gmail.com>
2023-11-11 19:54:09 +0700

    formik

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,60 +1,84 @@
-import { Button, Stack, TextField, Typography, styled } from "@mui/material";
+import { Stack, styled } from "@mui/material";
 import { Header } from "../../components";
-import { useState } from "react";
 import { login } from "../../services/account";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [loginInfo, setLoginInfo] = useState({
-    username: "",
-    password: "",
-  });
-  const [error, setError] = useState({
-    show: false,
-    text: "",
-  });
 
-  const onChangeLoginInfo = (updateData = {}) => {
-    setLoginInfo({ ...loginInfo, ...updateData });
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.username?.trim()) {
+      errors.username = "Required";
+    } else {
+      if (values.username?.length <= 3) {
+        errors.username = "Min length is 3 characters";
+      }
+    }
+
+    if (!values.password?.trim()) {
+      errors.password = "Required password";
+    }
+
+    return errors;
   };
 
-  const onSubmitLogin = (e) => {
-    e?.preventDefault();
-    console.log(loginInfo);
-    login(loginInfo)
+  const onSubmitLogin = (values) => {
+    login(values)
       .then((res) => {
         console.log(res);
         navigate("/");
       })
       .catch((err) => {
         console.log(err);
-        setError({ show: true, text: err?.response?.data?.msg });
+        formik.setErrors({ root: err?.response?.data?.msg });
       });
   };
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validate,
+    onSubmit: onSubmitLogin,
+  });
 
   return (
     <Main>
       <Header />
 
-      <form className="login_form" onSubmit={onSubmitLogin}>
+      <form className="login_form" onSubmit={formik?.handleSubmit}>
         <Stack spacing={2} p={2} maxWidth={460} mx="auto">
-          <TextField
+          <input
+            name="username"
             placeholder="Nhập tài khoản"
-            value={loginInfo.username}
-            onChange={(e) => onChangeLoginInfo({ username: e.target.value })}
-          />
-          <TextField
-            placeholder="Nhập mật khẩu"
-            value={loginInfo.password}
-            onChange={(e) => onChangeLoginInfo({ password: e.target.value })}
+            value={formik?.values?.username}
+            onChange={formik.handleChange}
           />
 
-          {error.show && (
-            <Typography className="error">{error.text}</Typography>
+          {formik.errors.username && (
+            <p style={{ color: "red" }}>{formik.errors.username}</p>
           )}
 
-          <Button type="submit">Login</Button>
+          <input
+            placeholder="Nhập mật khẩu"
+            name="password"
+            value={formik?.values?.password}
+            onChange={formik.handleChange}
+          />
+
+          {formik.errors.password && (
+            <p style={{ color: "red" }}>{formik.errors.password}</p>
+          )}
+
+          {formik.errors.root && (
+            <p style={{ color: "red" }}>{formik.errors.root}</p>
+          )}
+
+          <button type="submit">Login</button>
         </Stack>
       </form>
     </Main>
